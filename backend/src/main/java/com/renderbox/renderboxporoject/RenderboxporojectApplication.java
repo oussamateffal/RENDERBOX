294cb3ce4c90a01ad6f83ae7b1de9a39277223f3
Achraf AISSY <105090484+AissyAchraf@users.noreply.github.com>
2023-10-18 10:25:34 +0100

    init backend project

---
+++ backend/src/main/java/com/renderbox/renderboxporoject/RenderboxporojectApplication.java
@@ -0,0 +1,13 @@
+package com.renderbox.renderboxporoject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenderboxporojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenderboxporojectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/renderbox/renderboxporoject/RenderboxporojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renderbox.renderboxporoject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenderboxporojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
