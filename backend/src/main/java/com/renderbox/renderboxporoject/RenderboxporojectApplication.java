91b9222e9dfa5d1cae80848525cd739421553d2a
Achraf AISSY <105090484+AissyAchraf@users.noreply.github.com>
2023-11-26 20:31:27 +0100

    swagger APIs doc

--- backend/src/main/java/com/renderbox/renderboxporoject/RenderboxporojectApplication.java
+++ backend/src/main/java/com/renderbox/renderboxporoject/RenderboxporojectApplication.java
@@ -1,6 +1,10 @@
 package com.renderbox.renderboxporoject;
 
 import com.renderbox.renderboxporoject.config.JwtRequestFilter;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +12,22 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "RenderBox project API",
+				version = "1.0.0",
+				description = "Digital Agency Web Site",
+				termsOfService = "runcodenow",
+				contact = @Contact(
+						name = "Achraf Aissy",
+						email = "achrafaissy1@gmail.com"
+				),
+				license = @License(
+						name = "license",
+						url = "runcodenow"
+				)
+		)
+)
 public class RenderboxporojectApplication {
 
 	public static void main(String[] args) {
